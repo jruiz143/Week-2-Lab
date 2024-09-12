b6393a5453d732be420f4283610dd5e8ab97e126
Jackie <jruiz143@student.rccd.edu>
2024-09-11 21:26:43 -0700

    Add files via upload

---
+++ #PROGRAMMING EXERCISE #5.py
@@ -0,0 +1,12 @@
+#PROGRAMMING EXERCISE #5
+#A car is traveling 70 miles per hour. Write a program that displays:
+#The distance traveled in 6 hrs, 10hrs, and 15 hrs.
+
+speed= 70 #mph
+distance1= speed* 6 #hours
+distance2= speed* 10 #hours
+distance3= speed* 15 #hours
+#Display the different distances traveled
+print (f"The car traveled {distance1: .2f}' miles in 6 hours.")
+print (f"The car traveled {distance2: .2f}' miles in 6 hours.")
+print (f"The car traveled {distance3: .2f}' miles in 6 hours.")
